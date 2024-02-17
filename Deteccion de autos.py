16e7fa3c51c2c2e5eaec758c14f476b8a26b4f2e
JONATHAN DANIEL RODRIGUEZ AYALA <jonathan.rodriguez5651@alumnos.udg.mx>
2024-02-17 16:59:28 -0600

    Depuracion 2

--- Deteccion de autos.py
+++ Deteccion de autos.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 from ultralytics.solutions import object_counter
-import matplotlib.pyplot as plt
 import cv2
 
+# Cargar el modelo YOLOv8x y el video
 model = YOLO("yolov8x.pt")
 cap = cv2.VideoCapture("Video1.mp4")
 assert cap.isOpened(), "Error reading video file"
@@ -20,13 +20,13 @@ w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FR
 # # Define region points
 region_points = [(200, 500), (1200, 500)] #1 -----   #2 ----- 1
 
-# Video writer
+# Almacenamiento del video
 video_writer = cv2.VideoWriter("Resultado1.mp4",
                        cv2.VideoWriter_fourcc(*'mp4v'),
                        fps,
                        (w, h))
 
-# Init Object Counter
+# Contador de objetos
 counter = object_counter.ObjectCounter()
 counter.set_args(view_img=True,
                  reg_pts=region_points,
