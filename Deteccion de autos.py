791b14bc16bec383b24f71aa9dd34cc1df6665f6
JONATHAN DANIEL RODRIGUEZ AYALA <jonathan.rodriguez5651@alumnos.udg.mx>
2024-02-17 16:58:02 -0600

    Depuración 1

--- Deteccion de autos.py
+++ Deteccion de autos.py
@@ -3,24 +3,22 @@ from ultralytics.solutions import object_counter
 import matplotlib.pyplot as plt
 import cv2
 
-
 model = YOLO("yolov8x.pt")
 cap = cv2.VideoCapture("Video1.mp4")
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-# Leer el primer fotograma
-success, im0 = cap.read()
-if not success:
-    print("Unable to read video frame")
-
-# Mostrar el primer fotograma
-plt.imshow(cv2.cvtColor(im0, cv2.COLOR_BGR2RGB))
-plt.title('First Frame')
-plt.show()
-# Define region points
+#--------------Deteccion de nuestro primer pixel para trazar nuestro contador---#
+# # Leer el primer fotograma
+# success, im0 = cap.read()
+# if not success:
+#     print("Unable to read video frame")
+# # Mostrar el primer fotograma
+# plt.imshow(cv2.cvtColor(im0, cv2.COLOR_BGR2RGB))
+# plt.title('First Frame')
+# plt.show()
+# # Define region points
 region_points = [(200, 500), (1200, 500)] #1 -----   #2 ----- 1
-
 
 # Video writer
 video_writer = cv2.VideoWriter("Resultado1.mp4",
@@ -35,8 +33,6 @@ counter.set_args(view_img=True,
                  classes_names=model.names,
                  draw_tracks=False)
 
-    
-
 while cap.isOpened():
     success, im0 = cap.read()
     if not success:
@@ -47,7 +43,6 @@ while cap.isOpened():
     im0 = counter.start_counting(im0, tracks)
     video_writer.write(im0)             
     
-
 cap.release()
 video_writer.release()
 cv2.destroyAllWindows()
